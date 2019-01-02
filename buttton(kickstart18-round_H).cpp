fea0e98a33cf9986c834cb86ee35a4d7017555c5
sayan83 <42147789+sayan83@users.noreply.github.com>
2019-01-02 14:23:40 +0530

    Update buttton(kickstart18-round_H).cpp

--- buttton(kickstart18-round_H).cpp
+++ buttton(kickstart18-round_H).cpp
@@ -1,3 +1,17 @@
+/*
+
+You are a contestant on a popular new game show and are playing for the grand prize!
+
+There are two big buttons, a red one and a black one. You will make a sequence of exactly N button presses.
+
+There are lots of different sequences of presses you could make, but there are P forbidden prefixes, each of
+length no greater than N. If you make a sequence of presses which begins with any of the forbidden sequences, you 
+will not win the grand prize. It is fine for your sequence to contain one or more forbidden prefixes as long as they
+do not appear at the start of your sequence.
+
+A winning sequence must consist of exactly N button presses and must not begin with one of the forbidden prefixes. 
+How many different winning sequences are there?
+*/
 #include <iostream>
 #include <string>
 #include <math.h>

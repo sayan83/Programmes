331097c8a8852c3a82f3d0170dc72dfd36703145
sayan83 <42147789+sayan83@users.noreply.github.com>
2019-01-03 13:19:17 +0530

    Create ENTRANCE_XAM(CC).cpp

---
+++ ENTRANCE_XAM(CC).cpp
@@ -0,0 +1,17 @@
+/*
+The faculty of application management and consulting services (FAMCS) of the Berland State University (BSU) has 
+always been popular among Berland's enrollees. This year, N students attended the entrance exams, but no more 
+than K will enter the university. In order to decide who are these students, there are series of entrance exams. 
+All the students with score strictly greater than at least (N-K) students' total score gets enrolled.
+
+In total there are E entrance exams, in each of them one can score between 0 and M points, inclusively. 
+The first E-1 exams had already been conducted, and now it's time for the last tribulation.
+
+Sergey is the student who wants very hard to enter the university, so he had collected the information about
+the first E-1 from all N-1 enrollees (i.e., everyone except him). Of course, he knows his own scores as well.
+
+In order to estimate his chances to enter the University after the last exam, Sergey went to a fortune teller. 
+From the visit, he learnt about scores that everyone except him will get at the last exam. 
+Now he wants to calculate the minimum score he needs to score in order to enter to the university. 
+But now he's still very busy with minimizing the amount of change he gets in the shops, so he asks you to help him.
+*/

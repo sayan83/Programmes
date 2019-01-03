0f99669b8a04d4cf2fcfddb94b38440b53443dee
sayan83 <42147789+sayan83@users.noreply.github.com>
2019-01-03 13:20:05 +0530

    Update ENTRANCE_XAM(CC).cpp

--- ENTRANCE_XAM(CC).cpp
+++ ENTRANCE_XAM(CC).cpp
@@ -15,3 +15,50 @@ From the visit, he learnt about scores that everyone except him will get at the 
 Now he wants to calculate the minimum score he needs to score in order to enter to the university. 
 But now he's still very busy with minimizing the amount of change he gets in the shops, so he asks you to help him.
 */
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	long long int t,n,k,e,m;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n>>k>>e>>m;
+        long long int mks[n][e];
+        for(int i=0;i<n-1;i++)
+        {
+        	for(int j=0;j<e;j++)
+        		cin>>mks[i][j];
+        }
+        for(int i=0;i<e-1;i++)
+        	cin>>mks[n-1][i];
+        mks[n-1][e-1]=0;
+        long long int tm[n-1];
+        for(int i=0;i<n;i++)
+        {
+        	tm[i] = 0;
+        	for(int j=0;j<e;j++)
+        		tm[i]=tm[i]+mks[i][j];
+        }
+        for(int i=0;i<n-2;i++)
+    	{
+	       for(int j=i+1;j<n-1;j++)
+	       {
+	            if(tm[i]>tm[j])
+	            {
+	                long long int temp = tm[i];
+	                tm[i] = tm[j];
+	                tm[j] = temp;
+	            }
+	        }
+    	}
+        long long int mks_n=tm[n-k-1]+1 - tm[n-1];
+        if(mks_n > m)
+            cout<<"Impossible"<<endl;
+        else if(mks_n>=0)
+            cout<<mks_n<<endl;
+        else
+            cout<<0<<endl;
+	}
+	return 0;
+}
